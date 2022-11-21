22733a7f3f971e2a3f38276713fb2b3a0ca701fd
Hasby Rifky <hasbyalkaff@gmail.com>
2022-11-21 09:10:57 +0700

    first commit, ci3 with bootstrap

---
+++ application/views/template.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <meta charset="UtF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bank Economic System</title>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="<?=base_url('bootstrap-5.2.2-dist/css/bootstrap.min.css')?>" type="text/css" />
+    </head>
+    <body>
+        <div class="left-side"></div>
+        <div class="right-side"></div>
+
+        <!-- Bootstrap JS -->
+        <script src="<?=base_url('bootstrap-5.2.2-dist/js/bootstrap.min.js')?>" type="text/javascript" ></script>
+    </body>
+</html>
